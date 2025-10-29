24c2c6b0258eb343cdb5c3f18c8f19dcdf425adb
Surendra Yalakala <surendrayalakala@gmail.com>
2025-10-29 16:23:43 +0530

    database connection added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const express = require("express");
 
+const connectDB = require("./config/database");
+
 const app = express();
 
 //handler
-app.use("/test", (req, res) => {
-  res.send("Welcome to test");
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Surendra", lastName: "Yalakala", city: "Vijayawada" });
 });
-app.use("/home", (req, res) => {
-  res.send("Welcome to home");
+// app.get("/user/:userId/:firstName", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "Surendra", lastName: "Yalakala", city: "Vijayawada" });
+// });
+
+app.post("/user", (req, res) => {
+  res.send("Data inserted successfully");
 });
 
-// attching the port
-app.listen(7777, () => {
-  console.log("Server creation successfully done !!");
-});
+// app.use("/test", (req, res) => {
+//   res.send("Welcome to test");
+// });
+// app.use("/home", (req, res) => {
+//   res.send("Welcome to home");
+// });
+
+connectDB()
+  .then(() => {
+    console.log("Data base connection established successfully");
+
+    // attching the port
+    app.listen(7777, () => {
+      console.log("Server creation successfully done !!");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://learnvahinisurendra_db_user:3TEBwbP60SQPBgts@namastenode.8dfp8pn.mongodb.net/"
+  );
+};
+
+module.exports = connectDB;
