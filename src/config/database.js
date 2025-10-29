aa784735d3e602f5de7ffae30423d559baa5ff21
Surendra Yalakala <surendrayalakala@gmail.com>
2025-10-29 18:31:31 +0530

    removed database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://learnvahinisurendra_db_user:3TEBwbP60SQPBgts@namastenode.8dfp8pn.mongodb.net/devTinder"
-  );
+  await mongoose.connect("");
 };
 
 module.exports = connectDB;
