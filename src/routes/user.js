964223e257c9e89368ce8fe8ffabdb2e76ad7104
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-02 12:48:58 +0530

    connection api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const router = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName age gender photoUrl about skills";
+
 router.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -11,10 +13,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate(
-      "fromUserId",
-      "firstName lastName age gender photoUrl about skills"
-    );
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     console.log(loggedInUser, connectionRequest);
 
@@ -24,6 +23,31 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({ message: "Error received request " + error });
+  }
+});
+
+router.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({ data });
+  } catch (error) {
+    res.status(400).send({ message: "Error connections " + error });
   }
 });
 
