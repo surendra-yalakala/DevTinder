ba767565b835814495f01e81908922af48d81a12
Surendra Yalakala <surendrayalakala@gmail.com>
2025-11-05 11:15:37 +0530

    add db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("");
+  await mongoose.connect(
+    "mongodb+srv://learnvahinisurendra_db_user:3TEBwbP60SQPBgts@namastenode.8dfp8pn.mongodb.net/devTinder"
+  );
 };
 
 module.exports = connectDB;
